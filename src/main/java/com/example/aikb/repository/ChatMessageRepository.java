d759ecef188a4e4b1a2e59c3d4c64365779cad04
wenyang <417440818@qq.com>
2026-07-29 20:46:00 +0800

    111

--- src/main/java/com/example/aikb/repository/ChatMessageRepository.java
+++ src/main/java/com/example/aikb/repository/ChatMessageRepository.java
@@ -1,2 +1,12 @@
-package com.example.aikb.repository; import com.example.aikb.model.entity.ChatMessage; import org.springframework.data.jpa.repository.JpaRepository; import org.springframework.stereotype.Repository; import java.util.List;
-@Repository public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> { List<ChatMessage> findBySessionIdOrderByCreatedAtAsc(Long sessionId); }
+package com.example.aikb.repository;
+
+import com.example.aikb.model.entity.ChatMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+    List<ChatMessage> findBySessionIdOrderByCreatedAtAsc(Long sessionId);
+}
